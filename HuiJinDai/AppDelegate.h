c894a08f30c7c58b3254586a0f7a31bbe1110e7d
shanpx <shanpeixing@huankanxs.com>
2018-08-30 20:42:24 +0800

    新建工程

---
+++ HuiJinDai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuiJinDai
+//
+//  Created by 耿笑威 on 2018/8/30.
+//  Copyright © 2018年 shanpx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuiJinDaiTests/HuiJinDaiTests.m
@@ -0,0 +1,39 @@
+//
+//  HuiJinDaiTests.m
+//  HuiJinDaiTests
+//
+//  Created by 耿笑威 on 2018/8/30.
+//  Copyright © 2018年 shanpx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HuiJinDaiTests : XCTestCase
+
+@end
+
+@implementation HuiJinDaiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
